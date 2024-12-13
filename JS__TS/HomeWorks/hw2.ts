122444b7dd13a9344c4c68b14be7b07dadc132db
Liverpool4ik <liverpool4ik@proton.me>
2024-12-13 23:36:05 +0200

    homeWork_less__2

---
+++ JS__TS/HomeWorks/hw2.ts
@@ -0,0 +1,16 @@
+let x: string = "sss";
+let y: Number = 123;
+let z: boolean = false;
+
+console.log(x);
+console.log(y);
+
+let firstArray: number[] = [1, 223, 999]; //Array - a
+let secondArray: any[] = [x, y, z]; //Array - b
+let thirdArray: string[] = ["dddd", "fancyCar", "Life_is_Life na-na-Nana-na"]; //Array - c
+
+let x = thirdArray[0];
+let y = firstArray[firstArray.length - 1];
+
+console.log(x);
+console.log(y);
---
+++ JS__TS/lesson2.ts
@@ -0,0 +1,19 @@
+const Oldversion: Array<any> = ["1", "Anfield", 333, true];
+// Устаревший вид записи
+
+const newversion: any[] = ["22", "Stamford Bridge", 1987, false];
+// Современная вид записи
+
+const weKnowNumbers: [Number, Number, Number] = [111, 202, 399];
+// если мы точно знаем какой тип данных будет в массиве
+
+const weKnowDataTypesInArray: number[] = [111, 202, 399];
+// только ОДИН тип данных будет в массиве
+
+const number1 = 20;
+console.log(number1);
+console.log(number1);
+console.log(number1);
+console.log(number1);
+console.log(number1);
+console.log(number1);
