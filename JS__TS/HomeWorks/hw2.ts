34cd17fa2aa197e83dea57a7a887f07a924888f9
Liverpool4ik <liverpool4ik@proton.me>
2024-12-14 00:13:25 +0200

    Working at the lesson-3

--- JS__TS/HomeWorks/hw2.ts
+++ JS__TS/HomeWorks/hw2.ts
@@ -5,7 +5,7 @@ let z: boolean = false;
 console.log(x);
 console.log(y);
 
-let firstArray: number[] = [1, 223, 999]; //Array - a
+let firstArray: [number, number, number] = [1, 223, 999]; //Array - a
 let secondArray: any[] = [x, y, z]; //Array - b
 let thirdArray: string[] = ["dddd", "fancyCar", "Life_is_Life na-na-Nana-na"]; //Array - c
 
